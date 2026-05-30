52dfd07413ffe512bbb1389cc06be144fd7b1f68
USER1\nasti <nastialiubanievich@gmail.com>
2026-05-30 14:51:24 +0300

    зміна темної теми редагування вікна логін

--- CoffeeShopApp/Views/MenuView.xaml.cs
+++ CoffeeShopApp/Views/MenuView.xaml.cs
@@ -1,6 +1,8 @@
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using CoffeeShopApp.Data;
 using CoffeeShopApp.Models;
@@ -22,6 +24,16 @@ public partial class MenuView : UserControl
         LoadCategories();
         LoadProducts();
         ClearForm();
+        ApplyTheme();
+
+        Loaded += (_, _) => ApplyTheme();
+        Unloaded += (_, _) => AppSettings.Changed -= AppSettings_Changed;
+        AppSettings.Changed += AppSettings_Changed;
+    }
+
+    private void AppSettings_Changed(object? sender, EventArgs e)
+    {
+        ApplyTheme();
     }
 
     private void LoadCategories()
@@ -66,6 +78,112 @@ public partial class MenuView : UserControl
         }
 
         ProductsDataGrid.ItemsSource = products.ToList();
+        Dispatcher.BeginInvoke(ApplyTheme);
+    }
+
+    private void ApplyTheme()
+    {
+        var surfaceBrush = AppSettings.UseLightTheme
+            ? Brushes.White
+            : new SolidColorBrush(Color.FromRgb(58, 45, 32));
+        var alternateBrush = AppSettings.UseLightTheme
+            ? new SolidColorBrush(Color.FromRgb(251, 248, 244))
+            : new SolidColorBrush(Color.FromRgb(66, 52, 37));
+        var lineBrush = AppSettings.UseLightTheme
+            ? new SolidColorBrush(Color.FromRgb(231, 222, 210))
+            : new SolidColorBrush(Color.FromRgb(91, 74, 54));
+        var strongTextBrush = AppSettings.UseLightTheme
+            ? new SolidColorBrush(Color.FromRgb(23, 18, 14))
+            : new SolidColorBrush(Color.FromRgb(248, 246, 242));
+        var mutedTextBrush = AppSettings.UseLightTheme
+            ? new SolidColorBrush(Color.FromRgb(110, 98, 86))
+            : new SolidColorBrush(Color.FromRgb(216, 199, 179));
+
+        ProductsPanelBorder.Background = surfaceBrush;
+        ProductsPanelBorder.BorderBrush = lineBrush;
+        ProductFormBorder.Background = surfaceBrush;
+        ProductFormBorder.BorderBrush = lineBrush;
+
+        TitleTextBlock.Foreground = strongTextBrush;
+        SubtitleTextBlock.Foreground = mutedTextBrush;
+        FormTitleTextBlock.Foreground = strongTextBrush;
+        SearchPlaceholderTextBlock.Foreground = mutedTextBrush;
+
+        foreach (var textBlock in new[]
+                 {
+                     NameLabelTextBlock,
+                     CategoryLabelTextBlock,
+                     PriceLabelTextBlock,
+                     DescriptionLabelTextBlock,
+                     ImagePathLabelTextBlock
+                 })
+        {
+            textBlock.Foreground = mutedTextBrush;
+        }
+
+        foreach (var textBox in new[]
+                 {
+                     SearchTextBox,
+                     NameTextBox,
+                     PriceTextBox,
+                     DescriptionTextBox,
+                     ImagePathTextBox
+                 })
+        {
+            textBox.Background = AppSettings.UseLightTheme
+                ? Brushes.White
+                : new SolidColorBrush(Color.FromRgb(48, 39, 27));
+            textBox.Foreground = strongTextBrush;
+            textBox.BorderBrush = lineBrush;
+        }
+
+        CategoryComboBox.Background = AppSettings.UseLightTheme
+            ? Brushes.White
+            : new SolidColorBrush(Color.FromRgb(48, 39, 27));
+        CategoryComboBox.Foreground = strongTextBrush;
+        CategoryComboBox.BorderBrush = lineBrush;
+        AvailableCheckBox.Foreground = strongTextBrush;
+
+        ProductsDataGrid.Background = surfaceBrush;
+        ProductsDataGrid.Foreground = strongTextBrush;
+        ProductsDataGrid.RowBackground = surfaceBrush;
+        ProductsDataGrid.AlternatingRowBackground = alternateBrush;
+        ProductsDataGrid.BorderBrush = lineBrush;
+        ProductsDataGrid.HorizontalGridLinesBrush = lineBrush;
+        ProductsDataGrid.VerticalGridLinesBrush = Brushes.Transparent;
+        ProductsDataGrid.ColumnHeaderStyle = CreateColumnHeaderStyle(alternateBrush, mutedTextBrush, lineBrush);
+        ProductsDataGrid.CellStyle = CreateCellStyle(strongTextBrush);
+        ProductsDataGrid.RowStyle = CreateRowStyle(lineBrush);
+    }
+
+    private static Style CreateColumnHeaderStyle(Brush background, Brush foreground, Brush borderBrush)
+    {
+        var style = new Style(typeof(DataGridColumnHeader));
+        style.Setters.Add(new Setter(Control.BackgroundProperty, background));
+        style.Setters.Add(new Setter(Control.ForegroundProperty, foreground));
+        style.Setters.Add(new Setter(Control.BorderBrushProperty, borderBrush));
+        style.Setters.Add(new Setter(Control.BorderThicknessProperty, new Thickness(0, 0, 1, 1)));
+        style.Setters.Add(new Setter(Control.FontWeightProperty, FontWeights.SemiBold));
+        style.Setters.Add(new Setter(Control.PaddingProperty, new Thickness(6, 0, 6, 0)));
+        return style;
+    }
+
+    private static Style CreateCellStyle(Brush foreground)
+    {
+        var style = new Style(typeof(DataGridCell));
+        style.Setters.Add(new Setter(Control.ForegroundProperty, foreground));
+        style.Setters.Add(new Setter(Control.BackgroundProperty, Brushes.Transparent));
+        style.Setters.Add(new Setter(Control.BorderThicknessProperty, new Thickness(0)));
+        style.Setters.Add(new Setter(Control.PaddingProperty, new Thickness(4, 0, 4, 0)));
+        return style;
+    }
+
+    private static Style CreateRowStyle(Brush borderBrush)
+    {
+        var style = new Style(typeof(DataGridRow));
+        style.Setters.Add(new Setter(Control.BorderBrushProperty, borderBrush));
+        style.Setters.Add(new Setter(Control.BorderThicknessProperty, new Thickness(0, 0, 0, 1)));
+        return style;
     }
 
     private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
